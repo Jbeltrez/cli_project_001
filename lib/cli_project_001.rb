53e1b7986b4d407146e23beb4f75b6fa6926b6b0
Jbeltrez <beltrezjohn@gmail.com>
2020-08-21 17:04:05 -0400

    initial setup

---
+++ lib/cli_project_001.rb
@@ -0,0 +1,6 @@
+require "cli_project_001/version"
+
+module CliProject001
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_project_001/version.rb
@@ -0,0 +1,3 @@
+module CliProject001
+  VERSION = "0.1.0"
+end
